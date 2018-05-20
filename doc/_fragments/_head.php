02c3ee9b2566b0f6257060f32da3a350cc2fb882
Stéphane Chesné <contact@ogham.fr>
2018-05-20 22:56:06 +0200

    Correction du Head pour mobiles

--- doc/_fragments/_head.php
+++ doc/_fragments/_head.php
@@ -1,8 +1,13 @@
 <head>
-    <title>SipaUI</title>
     <meta charset="UTF-8" />
+    <title>SipaUI</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <!-- Favicon à faire -->
 
     <!-- Styles -->
     <link rel="stylesheet" href="/dist/css/default-demo.css" type="text/css" media="screen" charset="utf-8">
     <link rel="stylesheet" media="screen and (min-width: 64em)" href="/dist/css/large-demo.css" type="text/css" media="screen" charset="utf-8">
 </head>
+
