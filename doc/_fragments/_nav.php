cda23edac9be936228cd130cdb63dc93cbb52216
Stéphane Chesné <contact@ogham.fr>
2018-05-20 22:44:04 +0200

    Refonte nav

--- doc/_fragments/_nav.php
+++ doc/_fragments/_nav.php
@@ -1,6 +1,6 @@
 <ul class="su-horizontal">
-    <li><a href="/doc/architecture.php" <?php if($actif=='archi') echo 'class="actif"' ?>>Architecture</a></li>
-    <li><a href="/doc/avancement.php" <?php if($actif=='avancement') echo 'class="actif"' ?>>Avancement</a></li>
+    <li><a href="/doc/architecture.php" <?php if($actif=='archi') echo 'class="actif"' ?>>Archi</a></li>
+    <li><a href="/doc/avancement.php" <?php if($actif=='avancement') echo 'class="actif"' ?>>Suivi</a></li>
     <li><a href="/doc/poc/index.php" <?php if($actif=='poc') echo 'class="actif"' ?>>POC</a></li>
-    <li><a href="/dist/storybook">Storybook</a></li>
+    <li><a href="/dist/storybook">Catalogue</a></li>
 </ul>
