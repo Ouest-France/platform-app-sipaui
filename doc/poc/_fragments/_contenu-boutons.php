86174ed373ba0942f4037f8dc5a1700f127e9700
Ogham <contact@ogham.fr>
2018-08-27 17:08:57 +0200

    [DOC] Refonte gestion des titres de la Doc en POC

--- doc/poc/_fragments/_contenu-boutons.php
+++ doc/poc/_fragments/_contenu-boutons.php
@@ -1,11 +1,20 @@
 <article class="conteneur" role="article">
-                <h1>Boutons <?php if($template==true) echo $template ?></h1>
-                <h2>Standards</h2>
+                <h1 class="titre-doc">Boutons <?php if($template==true) echo $template ?></h1>
+                <h2 class="titre-doc">Standards</h2>
                 <p class="su-margin-b-standard">Chaque bouton a 4 états : normal (/ ou <code>link</code>), survolé (<code>hover</code>), cliqué (<code>active</code>) et visité (<code>visited</code>)</p>
                 <ul class="liste-boutons">
                     <li>Bouton <code>basic</code>
-                        <div>
-                            <a class="su-button" href="#">Bouton</a>
+                        <p>Balise <code>&lt;a&gt;</code></p>
+                        <div class="su-margin-b-small">
+                            <a class="su-button" href="#">Bouton &lt;a&gt;</a>
+                        </div>
+                        <p>Balise <code>&lt;input&gt;</code></p>
+                        <div class="su-margin-b-small">
+                            <input class="su-button" type="button" value="Bouton &lt;input&gt;">
+                        </div>
+                        <p>Balise <code>&lt;button&gt;</code></p>
+                        <div class="su-margin-b-small">
+                            <button class="su-button" href="#">Bouton &lt;button&gt;</button>
                         </div>
                     </li>
                     <li>Bouton <code>primary</code>
@@ -25,7 +34,7 @@
                     </li>
                 </ul>
 
-                <h2>Tailles</h2>
+                <h2 class="titre-doc">Tailles</h2>
                 <p class="su-margin-b-standard">Chaque taille peut être appliquée à chaque type de bouton.</p>
                 <ul class="liste-boutons">
                     <li>Bouton <code>basic</code>
@@ -45,7 +54,7 @@
                     </li>
                 </ul>
 
-                <h2>Fluides</h2>
+                <h2 class="titre-doc">Fluides</h2>
                 <ul class="su-not-list su-margin-b-standard liste-boutons">
                     <li>Bouton <code>fluid</code>
                         <div>
--- doc/poc/_fragments/_contenu-texte.php
+++ doc/poc/_fragments/_contenu-texte.php
@@ -1,20 +1,18 @@
 <article class="conteneur" role="article">
-                <h1>Textes <?php if($template==true) echo $template ?></h1>
+                <h1 class="titre-doc">Textes <?php if($template==true) echo $template ?></h1>
 
-                <h2>Titres</h2>
+                <h2 class="titre-doc">Titres</h2>
 
-                <h3>Titres standards</h3>
+                <h3 class="titre-doc">Titres standards</h3>
 
-                <div class="pas-editorial su-margin-b-large">
-                    <p class="su-h1">Titre <code>h1</code> ou <code>class="su-h1"</code></p>
-                    <p class="su-h2">Titre <code>h2</code> ou <code>class="su-h2"</code></p>
-                    <p class="su-h3">Titre <code>h3</code> ou <code>class="su-h3"</code></p>
-                    <p class="su-h4">Titre <code>h4</code> ou <code>class="su-h4</code></p>
-                    <p class="su-h5">Titre <code>h5</code> ou <code>class="su-h5"</code></p>
-                    <p class="su-h6">Titre <code>h6</code> ou <code>class="su-h6"</code></p>
-                </div>
+                <p class="su-h1">Titre <code>h1</code> ou <code>class="su-h1"</code></p>
+                <p class="su-h2">Titre <code>h2</code> ou <code>class="su-h2"</code></p>
+                <p class="su-h3">Titre <code>h3</code> ou <code>class="su-h3"</code></p>
+                <p class="su-h4">Titre <code>h4</code> ou <code>class="su-h4</code></p>
+                <p class="su-h5">Titre <code>h5</code> ou <code>class="su-h5"</code></p>
+                <p class="su-h6">Titre <code>h6</code> ou <code>class="su-h6"</code></p>
 
-                <h3>Titres éditoriaux avec <code>class="su-editorial"</code> sur un parent</h3>
+                <h3 class="titre-doc">Titres éditoriaux avec <code>class="su-editorial"</code> sur lui-même ou sur un parent</h3>
 
                 <p class="su-h1 su-editorial">Titre <code>h1</code> ou <code>class="su-h1"</code></p>
                 <p class="su-h2 su-editorial">Titre <code>h2</code> ou <code>class="su-h2"</code></p>
@@ -25,30 +23,30 @@
 
                 <hr/><!--------------------------------------------------------------------------------- -->
 
-                <h2>Paragraphes</h2>
+                <h2 class="titre-doc">Paragraphes</h2>
 
-                <h3>Standards ou avec <code>class="su-line-height-base"</code></h3>
+                <h3 class="titre-doc">Standards ou avec <code>class="su-line-height-base"</code></h3>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
-                <h3>Avec <code>class="su-line-height-small"</code></h3>
+                <h3 class="titre-doc">Avec <code>class="su-line-height-small"</code></h3>
 
                 <p class="su-line-height-small">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
-                <h3>Avec <code>class="su-line-height-xsmall"</code></h3>
+                <h3 class="titre-doc">Avec <code>class="su-line-height-xsmall"</code></h3>
 
                 <p class="su-line-height-xsmall">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
                 <hr/><!--------------------------------------------------------------------------------- -->
 
-                <h2>Emphase</h2>
+                <h2 class="titre-doc">Emphase</h2>
 
-                <h3>Gras <code>strong</code> avec la classe <code>class="su-important"</code></h3>
+                <h3 class="titre-doc">Gras <code>strong</code> avec la classe <code>class="su-important"</code></h3>
                 <p>Lorem ipsum <strong class="su-important">dolor sit amet</strong>.</p>
 
-                <h3>Gras <code>strong</code></h3>
+                <h3 class="titre-doc">Gras <code>strong</code></h3>
                 <p>Lorem ipsum <strong>dolor sit amet</strong>.</p>
 
-                <h3>Italique <code>em</code></h3>
+                <h3 class="titre-doc">Italique <code>em</code></h3>
                 <p>Lorem ipsum <em>dolor sit amet</em>.</p>
             </article>
