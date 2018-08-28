fcaf53aa84f20a425103d53ae973395d5153b7f8
Ogham <contact@ogham.fr>
2018-08-28 15:41:23 +0200

    [DOC] Standardisation de la designation du code dans la doc

--- doc/poc/_fragments/_contenu-boutons.php
+++ doc/poc/_fragments/_contenu-boutons.php
@@ -3,19 +3,27 @@
                 <h2 class="titre-doc">Standards</h2>
                 <p class="su-margin-b-standard">Chaque bouton possède 4 états HTML standards&nbsp;: normal, survolé (<code>hover</code>), cliqué (<code>active</code>), et avec focus (<code>focus</code>)</p>
                 <ul class="liste-boutons">
-                    <li><h3 class="titre-doc">Bouton <code>basic</code></h3>
-                        <p>Balise <code>&lt;a&gt;</code></p>
-                        <div class="su-margin-b-small">
-                            <a class="su-button" href="#">Bouton &lt;a&gt;</a>
-                        </div>
-                        <p>Balise <code>&lt;input&gt;</code></p>
-                        <div class="su-margin-b-small">
-                            <input class="su-button" type="button" value="Bouton &lt;input&gt;">
-                        </div>
-                        <p>Balise <code>&lt;button&gt;</code></p>
-                        <div class="su-margin-b-small">
-                            <button class="su-button" tabindex="0" href="#">Bouton &lt;button&gt;</button>
-                        </div>
+                    <li><h3 class="titre-doc">Bouton basic <code>.su-button</code></h3>
+                        <ul>
+                            <li>
+                                <p>Balise <code>&lt;a&gt;</code></p>
+                                <div class="su-margin-b-small">
+                                    <a class="su-button" href="#">Bouton &lt;a&gt;</a>
+                                </div>
+                            </li>
+                            <li>
+                                <p>Balise <code>&lt;input&gt;</code></p>
+                                <div class="su-margin-b-small">
+                                    <input class="su-button" type="button" value="Bouton &lt;input&gt;">
+                                </div>
+                            </li>
+                            <li>
+                                <p>Balise <code>&lt;button&gt;</code></p>
+                                <div class="su-margin-b-small">
+                                    <button class="su-button" tabindex="0" href="#">Bouton &lt;button&gt;</button>
+                                </div>
+                            </li>
+                        </ul>
                     </li>
                     <!--li><h3 class="titre-doc">Bouton <code>primary</code></h3>
                         <div>
