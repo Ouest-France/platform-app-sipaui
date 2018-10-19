13f98f921cff116492f075823e3989808e008a3c
Ogham <contact@ogham.fr>
2018-10-19 16:43:58 +0200

    [DOC] Target blank sur le catalogue dans la nav.

--- doc/_fragments/_nav.php
+++ doc/_fragments/_nav.php
@@ -3,5 +3,5 @@
     <li><a href="/doc/architecture-technique.php" <?php if($actif=='tech') echo 'class="actif"' ?>>Architecture technique</a></li>
     <li><a href="/doc/telechargement.php" <?php if($actif=='telechargement') echo 'class="actif"' ?>>Téléchargement</a></li>
     <li><a href="/doc/suivi.php" <?php if($actif=='suivi') echo 'class="actif"' ?>>Suivi</a></li>
-    <li><a href="/dist/storybook">Catalogue</a></li>
+    <li><a href="/dist/storybook" target="_blank">Catalogue</a></li>
 </ul>
