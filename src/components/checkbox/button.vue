8541ae854684f70a6240be1174f164cf92d4558a
Colin Céline <pestasin84@ouest-france.fr>
2018-11-15 10:18:00 +0100

    [COMP] Màj Doc design Bouton et Checkbox

---
+++ src/components/checkbox/button.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="su-old-button" :class="[typeClass]">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    typeClass: 'basic'
+  }
+};
+</script>
+
+<style>
+/**
+Import du SASS button Sipa UI
+**/
+</style>
