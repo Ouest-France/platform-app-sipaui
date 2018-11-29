512f827bd68c2053f97ceadfdde70435c3ada527
Ogham <contact@ogham.fr>
2018-11-29 17:01:34 +0100

    Corrections suite Rebase de Develop

---
+++ src/components/margins/margins.vue
@@ -0,0 +1,20 @@
+<template>
+  <margins class="su-margins" :class="[typeClass]">
+    <slot></slot>
+  </margins>
+</template>
+
+<script>
+export default {
+  props: {
+    typeClass: 'basic'
+  }
+};
+</script>
+
+<style>
+/**
+Import du SASS margins Sipa UI
+**/
+@import margins.scss
+</style>
