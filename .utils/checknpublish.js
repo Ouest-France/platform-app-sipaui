9dfb1302c137d5326970e76ab4b5dd3f0e05f4cb
Presse Matthieu <matthieu.presse@ouest-france.fr>
2018-12-27 12:17:25 +0100

    tech: check n publish to npm

---
+++ .utils/checknpublish.js
@@ -0,0 +1,27 @@
+var exec = require('child_process').exec
+var fs = require("fs");
+
+var content = fs.readFileSync("./package.json");
+if(!content || !JSON.parse(content) || !JSON.parse(content).version) {
+    console.log("error reading version from package");
+}
+var child = exec('npm show sipa-ui version',
+  function(err, stdout, stderr) {
+    if (err) throw err;
+    else {
+        var distant = stdout.trim();
+        var local = JSON.parse(content).version;
+
+        if(distant != local) {
+            exec('npm publish',
+              function(err, stdout, stderr) {
+                if (err) throw err;
+                else {
+                    console.log('Version ', local, ' published !');
+                }
+            });
+        } else {
+            console.log('Nothing new to publish');
+        }
+    }
+});
