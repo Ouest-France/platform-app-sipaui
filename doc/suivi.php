075e8b9ca1d520bbdbf63c60f8324ab1ffb378c3
Ogham <contact@ogham.fr>
2019-02-08 09:47:24 +0100

    [SITE] Màj suivi

--- doc/suivi.php
+++ doc/suivi.php
@@ -24,18 +24,41 @@
                 <section class="integre" role="article">
                     <h2>Liste des composants intégrés</h2>
 
+                    <h3>Composants</h3>
                     <ul>
                     	<li>Boutons</li>
-                    	<li>Champs texte (input texte, textarea)
+                    	<li>Titres</li>
                     	<li>Liens</li>
-                    	<li>Helpers (classes d’aide)</li>
+                    	<li>Champs texte (input texte, textarea)</li>
                     	<li>Boutons radio</li>
                     	<li>Cases à cocher</li>
                     	<li>Select simple</li>
+                    </ul>
+
+                    <h3>Fondations</h3>
+                    <ul>
+                    	<li>Points de rupture</li>
+                    	<li>Grille</li>
                     	<li>Marges</li>
                     	<li>Texte standard</li>
-                    	<li>Titres</li>
                     	<li>Police d’icônes</li>
+                    	<li>Helpers (classes d’aide)</li>
+                    </ul>
+                </section>
+
+                <section class="plan-versions" role="article">
+                    <h2>Journal des modifications</h2>
+
+                    <ul class="su-old-not-list table">
+                    	<li class="row">
+                    	    <div class="cell"><span class="version">1.0</span></div>
+                    	    <ul class="cell">
+                        	    <li>Nouveaux composants&nbsp;: points de rupture et grille.</li>
+                        	    <li>Réorganisation des composants dans le catalogue.</li>
+                                <li>Améliorations graphiques et ergonomiques du catalogue.</li>
+                                <li>Quelques corrections de composants.</li>
+                            </ul>
+                        </li>
                     </ul>
                 </section>
 
@@ -43,13 +66,9 @@
                     <h2>Plan de versions prévisionnel</h2>
 
                     <ul class="su-old-not-list table">
-                    	<li class="row">
-                    	    <div class="cell"><span class="version">1.0</span></div>
-                    	    <div class="cell">Points de rupture et grille.</div>
-                        </li>
                         <li class="row">
                     	    <div class="cell"><span class="version">1.1</span></div>
-                    	    <div class="cell">Loader de page et listes.</div>
+                    	    <div class="cell">Listes et loader de page.</div>
                         </li>
                     	<li class="row">
                     	    <div class="cell"><span class="version">1.2</span></div>
