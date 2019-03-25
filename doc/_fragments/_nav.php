5f65c864c758d4bc99c685d209b49869409db4d7
Ogham <contact@ogham.fr>
2019-03-25 17:59:13 +0100

    [SITE] Nav : renommage « Catalogue » en « Composants »

--- doc/_fragments/_nav.php
+++ doc/_fragments/_nav.php
@@ -3,5 +3,5 @@
     <li><a href="/doc/architecture-fonctionnelle.php" <?php if($actif=='archi') echo 'class="actif"' ?>>Architecture fonctionnelle</a></li>
     <li><a href="/doc/architecture-technique.php" <?php if($actif=='tech') echo 'class="actif"' ?>>Architecture technique</a></li>
     <li><a href="/doc/suivi.php" <?php if($actif=='suivi') echo 'class="actif"' ?>>Suivi</a></li>
-    <li><a href="/dist/storybook" target="_blank">Catalogue</a></li>
+    <li><a href="/dist/storybook" target="_blank">Composants</a></li>
 </ul>
