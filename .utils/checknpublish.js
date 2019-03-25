1d1e39fc5c59efad9941c751357b884359a5fc66
Presse Matthieu <matthieu.presse@ouest-france.fr>
2019-03-25 12:00:50 +0100

    tech() 1er essaie cdn

--- .utils/checknpublish.js
+++ .utils/checknpublish.js
@@ -12,14 +12,26 @@ var child = exec('npm show sipa-ui version',
         var distant = stdout.trim();
         var local = JSON.parse(content).version;
         console.log('check version', distant, local);
-        if(distant != local) {
-            exec('npm publish',
-              function(err, stdout, stderr) {
-                if (err) throw err;
-                else {
-                    console.log('Version ', local, ' published !');
-                }
+        // if(distant != local) {
+        if(true) {
+            [
+                'npm publish',
+                'rsync -avz dist/js sshacs@static-of.scp.upload.akamai.com:/487320/sipaui/latest',
+                'rsync -avz dist/css sshacs@static-of.scp.upload.akamai.com:/487320/sipaui/latest',
+                'rsync -avz dist/fonts sshacs@static-of.scp.upload.akamai.com:/487320/sipaui/latest',
+                'rsync -avz dist/js sshacs@static-of.scp.upload.akamai.com:/487320/sipaui/' + local,
+                'rsync -avz dist/css sshacs@static-of.scp.upload.akamai.com:/487320/sipaui/' + local,
+                'rsync -avz dist/fonts sshacs@static-of.scp.upload.akamai.com:/487320/sipaui/' + local,
+            ].forEach(cmd => {
+                exec(cmd,
+                  function(err, stdout, stderr) {
+                    if (err) throw err;
+                    else {
+                        console.log('Version ', local, ' published !');
+                    }
+                });
             });
+
         } else {
             console.log('Nothing new to publish');
         }
