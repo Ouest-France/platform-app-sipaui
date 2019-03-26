4aaf4d12890ddbee0537d68872c3f03591c91c18
Ogham <contact@ogham.fr>
2019-03-26 20:27:59 +0100

    [SITE] Suppression de l’archi technique, déplacée dans le readme

--- doc/_fragments/_nav.php
+++ doc/_fragments/_nav.php
@@ -1,7 +1,6 @@
 <ul class="su-old-horizontal"><?php if(!isset($actif)) $actif = ''; ?>
     <li><a href="/doc/installation.php" <?php if($actif=='installation') echo 'class="actif"' ?>>Installation</a></li>
     <li><a href="/doc/architecture-fonctionnelle.php" <?php if($actif=='archi') echo 'class="actif"' ?>>Architecture fonctionnelle</a></li>
-    <li><a href="/doc/architecture-technique.php" <?php if($actif=='tech') echo 'class="actif"' ?>>Architecture technique</a></li>
     <li><a href="/doc/journal.php" <?php if($actif=='journal') echo 'class="actif"' ?>>Journal</a></li>
     <li><a href="/doc/route.php" <?php if($actif=='route') echo 'class="actif"' ?>>Feuille de route</a></li>
     <li><a href="/dist/storybook" target="_blank">Composants</a></li>
