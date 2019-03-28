94d5dd0d6b520d6647e44ee73eb56bd102ae3286
Ogham <contact@ogham.fr>
2019-03-28 10:28:04 +0100

    [SITE] Journal : concaténation avec « route »

--- doc/_fragments/_nav.php
+++ doc/_fragments/_nav.php
@@ -2,6 +2,5 @@
     <li><a href="/doc/installation.php" <?php if($actif=='installation') echo 'class="actif"' ?>>Installation</a></li>
     <li><a href="/doc/architecture-fonctionnelle.php" <?php if($actif=='archi') echo 'class="actif"' ?>>Architecture fonctionnelle</a></li>
     <li><a href="/doc/journal.php" <?php if($actif=='journal') echo 'class="actif"' ?>>Journal</a></li>
-    <li><a href="/doc/route.php" <?php if($actif=='route') echo 'class="actif"' ?>>Feuille de route</a></li>
     <li><a href="/dist/storybook" target="_blank">Composants</a></li>
 </ul>
