e663c25e5fb856a68216d36ddbe2942315879ef0
Ogham <contact@ogham.fr>
2019-04-25 14:48:07 +0200

    [Site] Màj Journal

--- doc/journal.php
+++ doc/journal.php
@@ -28,13 +28,13 @@
 
                         <ul class="su-not-list">
                             <li class="table-row">
-                                <div class="table-cell"><span class="version">1.2.2</span></div>
+                                <div class="table-cell"><span class="version">1.2.1</span></div>
                                 <ul class="table-cell">
                                     <li>Composants&nbsp;:
                                         <ul>
                                             <li>Petit texte</li>
                                             <li>Mutualisation des sous-thèmes «&nbsp;abonnement&nbsp;» et «&nbsp;premium&nbsp;»</li>
-                                            <li>Stepper</li>
+                                            <!--li>Stepper</li-->
                                         </ul>
                                     </li>
                                 </ul>
