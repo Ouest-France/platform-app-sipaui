3388b2b8a7a6ce09af0b3a81ec75d6ba143eb970
David Landais <prestasin75@ouest-france.fr>
2019-06-17 10:16:40 +0200

    feature/vue ajout de la définition du composant su-button

---
+++ src/components/COMPONENTS/Buttons/button.vue
@@ -0,0 +1,71 @@
+<script>
+import Vue from 'vue';
+export default Vue.extend({
+  props: {
+    tag: {
+      type: String,
+      required: false,
+      default: 'button',
+    },
+    title: {
+      type: String,
+      required: false,
+      default: undefined,
+    },
+    href: {
+      type: String,
+      required: false,
+      default: undefined,
+    },
+    primary: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    secondary: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    small: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    disabled: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  computed: {
+    classnames() {
+      return {
+        'su-button': true,
+        'su-primary': this.primary,
+        'su-secondary': this.secondary,
+        'su-small': this.small,
+        'su-disabled': this.disabled,
+      };
+    },
+  },
+  render(createElement) {
+    const children = this.$slots.default || this.title;
+    const options = {
+      on: this.$listeners,
+      class: this.classnames,
+      attrs: {
+        href: this.href,
+        title: this.title,
+        disabled: this.disabled,
+      }
+    };
+    return createElement(this.tag, options, children);
+  }
+});
+</script>
+
+<style lang="scss">
+@import "../../../core/scss/variables";
+@import "./main";
+</style>
