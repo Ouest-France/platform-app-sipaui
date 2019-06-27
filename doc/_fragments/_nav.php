35919a3f6f9f71a96af7e72fde6ee9cfb2e98593
Ogham <contact@ogham.fr>
2019-06-27 16:55:13 +0200

    [SITE] Suppression de old-sipaui

--- doc/_fragments/_nav.php
+++ doc/_fragments/_nav.php
@@ -1,4 +1,4 @@
-<ul class="su-old-horizontal su-container"><?php if(!isset($actif)) $actif = ''; ?>
+<ul class="su-row su-grid-auto su-wrap su-container"><?php if(!isset($actif)) $actif = ''; ?>
     <li><a href="/doc/installation.php" <?php if($actif=='installation') echo 'class="actif"' ?>>Installation</a></li>
     <li><a href="/doc/architecture-fonctionnelle.php" <?php if($actif=='archi') echo 'class="actif"' ?>>Architecture fonctionnelle</a></li>
     <li><a href="/doc/journal.php" <?php if($actif=='journal') echo 'class="actif"' ?>>Journal</a></li>
