e1ea42ed5ac6464b46ebc1ccc5faeb75e522726a
prestasin69 <prestasin69@ouest-france.fr>
2019-07-04 18:33:14 +0200

    [SUI-103] Fix chargement de la page

--- src/core/js/libs/components/supasswords.js
+++ src/core/js/libs/components/supasswords.js
@@ -3,36 +3,42 @@
 //
 // -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~
 ;(function(cn){
-    const password = "su-password",
-        passwordtoggle = "su-password-toggle",
-        conditions = "su-conditions",
+    const password = ".su-password",
+        passwordtoggle = ".su-password-toggle",
+        conditions = ".su-conditions",
         condition_error = "su-conditions-error",
         condition_success = "su-conditions-success",
         minchar = 8;
 
-    const conts = su.doc.querySelectorAll("."+password);
-    conts.forEach(function(cont){
-        const input = cont.querySelector("input");
-        if (!cont || !input || !cont.querySelector("."+conditions)) return;
-        const konditions = cont.querySelector("."+conditions);
-        _checkPassword(e,input,konditions);
-    })
+    function _launchDetection() {
+        const containers = su.doc.querySelectorAll(password);
 
+        containers.forEach(cont => {
+            let input = cont.querySelector("input");
+            if (!input || !cont.querySelector(conditions)) return;
+            let konditions = cont.querySelector(conditions);
+            _checkPassword(input,konditions);
+        });
+    }
+    
     function _togglePassword(cont) {
         let elem = cont.querySelector('input');
         if(elem.getAttribute("type") == "password") {
             elem.setAttribute("type","text");
+            cont.classList.add('su-js-show','su-toggle-text');
         } else {
             elem.setAttribute("type","password");
+            cont.classList.remove('su-js-show','su-toggle-text');
         }
+        elem.focus();
     }
 
-    function _checkPassword(e,input,konditions) {
+    function _checkPassword(input,konditions) {
         let conf_conditions = 
         [
             {
                 "klass":"su-condition-character-length",
-                "regexp":"8"
+                "regexp":".{8,}"
             },
             {
                 "klass":"su-condition-uppercase",
@@ -53,52 +59,59 @@
     }
 
     function _testValue(value,o) {
-        if(o.klass == "su-condition-character-length") {
-            if(value.length < minchar) {
-                return false
-            } else {
-                return true;
-            }
-        }
-        let regexp = new RegExp(o.regexp, "g");
-        if(regexp.test(value)) {
-            return true;
-        } else {
-            return false;
-        }
+        return new RegExp(o.regexp, "g").test(value);
     }
 
     function _conditionCheck(konditions,klass,response) {
         let char_condition = konditions.querySelector('.'+klass);
         char_condition.classList.remove(condition_error,condition_success);
-        if(response) {
-            char_condition.classList.add(condition_success);
-        } else {
-            char_condition.classList.add(condition_error);
-        }
+        char_condition.classList.add(response ? condition_success : condition_error);
     }
 
     function _stayFocus(cont) {
         cont.classList.add('su-password-focus');
     }
 
+    function _focusManagement(cont) {
+        if(cont.classList.contains('su-toggle-text')) return;
+        cont.classList.toggle('su-js-show');
+    }
+
     su.doc.addEventListener('click', function(e) {
-        const cont = e.target.closest("."+password);
-        const button = e.target.closest("."+passwordtoggle);
+        const cont = e.target.closest(password);
+        const button = e.target.closest(passwordtoggle);
         if(!cont) return;
         _stayFocus(cont);
-        if (!button) return;
+        if(!button) return;
         _togglePassword(cont);
     });
 
-    ['keypress','keyup'].forEach(function(event) {
+    ['input'].forEach(function(event) {
         su.doc.addEventListener(event, function(e) {
-            const cont = e.target.closest("."+password);
+            const cont = e.target.closest(password);
             const input = e.target.closest("input");
-            if (!cont || !input || !cont.querySelector("."+conditions)) return;
-            const konditions = cont.querySelector("."+conditions);
-            _checkPassword(e,input,konditions);
+            if (!cont || !input || !cont.querySelector(conditions)) return;
+            const konditions = cont.querySelector(conditions);
+            _checkPassword(input,konditions);
         });
     });
+
+    ['focusin','focusout'].forEach(function(event) {
+        su.doc.addEventListener(event, function(e) {
+            const cont = e.target.closest(password);
+            const input = e.target.closest("input");
+            if (!cont || !input) return;
+            _focusManagement(cont);
+        });
+    });
+
+    if( su.doc.readyState == 'complete') {
+        _launchDetection();
+    } else {
+        su.doc.addEventListener('readystatechange', function() {
+            if( su.doc.readyState != 'complete') return;
+            _launchDetection();
+        });
+    }
 
 })('supasswords');
