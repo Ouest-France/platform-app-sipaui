c1a7e40cca87456529b330370cbf1153f47531cc
Ogham <contact@ogham.fr>
2019-07-04 18:31:50 +0200

    Corrections post Merge

--- src/components/COMPONENTS/Inputs/Buttons/button.vue
+++ src/components/COMPONENTS/Inputs/Buttons/button.vue
@@ -1,17 +1,28 @@
-<template>
-  <button
-    class="su-button"
-    :class="classnames"
-    :disabled="disabled"
-  >
-    <slot />
-  </button>
-</template>
-
 <script>
-export default {
+import Vue from 'vue';
+export default Vue.extend({
   props: {
-    disabled: {
+    tag: {
+      type: String,
+      required: false,
+      default: 'button',
+    },
+    title: {
+      type: String,
+      required: false,
+      default: undefined,
+    },
+    href: {
+      type: String,
+      required: false,
+      default: undefined,
+    },
+    primary: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    secondary: {
       type: Boolean,
       required: false,
       default: false,
@@ -21,36 +32,40 @@ export default {
       required: false,
       default: false,
     },
-    fullwidth: {
+    disabled: {
       type: Boolean,
       required: false,
       default: false,
     },
-    wide: {
-      type: Boolean,
-      required: false,
-      default: false,
-    },
-    mobile: {
-      type: Boolean,
-      required: false,
-      default: false,
-    }
   },
   computed: {
     classnames() {
       return {
-        'su-disabled': this.disabled,
+        'su-button': true,
+        'su-primary': this.primary,
+        'su-secondary': this.secondary,
         'su-small': this.small,
-        'su-fullwidth': this.wide && !this.mobile,
-        'su-fullwidth-mobile': this.wide && this.mobile,
+        'su-disabled': this.disabled,
+      };
+    },
+  },
+  render(createElement) {
+    const children = this.$slots.default || this.title;
+    const options = {
+      on: this.$listeners,
+      class: this.classnames,
+      attrs: {
+        href: this.href,
+        title: this.title,
+        disabled: this.disabled,
       }
-    }
+    };
+    return createElement(this.tag, options, children);
   }
-};
+});
 </script>
 
 <style lang="scss">
-@import "../../core/scss/variables";
+@import "../../../core/scss/variables";
 @import "./main";
 </style>
