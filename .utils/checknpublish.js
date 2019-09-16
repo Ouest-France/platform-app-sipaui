3608fbc072dfb792f274026a29aef7b8ba807f58
Presse Matthieu <matthieu.presse@ouest-france.fr>
2019-09-16 12:01:39 +0200

    fix() remove dependancies in npm package

--- .utils/checknpublish.js
+++ .utils/checknpublish.js
@@ -1,18 +1,27 @@
 var exec = require('child_process').exec
 var fs = require("fs");
 
-var content = fs.readFileSync("./package.json");
-if(!content || !JSON.parse(content) || !JSON.parse(content).version) {
+var package_content = fs.readFileSync("./package.json");
+if(!package_content || !JSON.parse(package_content) || !JSON.parse(package_content).version) {
     console.log("error reading version from package");
 }
+package_content = JSON.parse(package_content);
+
 var child = exec('npm show sipa-ui version',
   function(err, stdout, stderr) {
     if (err) throw err;
     else {
         var distant = stdout.trim();
-        var local = JSON.parse(content).version;
+        var local = package_content.version;
         console.log('check version', distant, local);
         if(distant != local) {
+
+            package_content.devDependencies = {};
+            package_content.dependencies = {};
+            package_content.scripts = {"test": "echo \"Error: no test specified\" && exit 1"};
+
+            fs.writeFileSync("./package.json", JSON.stringify(package_content, null, 2));
+
             [
                 'npm publish',
                 'rsync -avz dist/js sshacs@static-of.scp.upload.akamai.com:/487320/sipaui/latest',
