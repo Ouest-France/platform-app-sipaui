14ba41ca352ccf9e655efe5c3e89212239341e8c
Ogham <contact@ogham.fr>
2019-11-21 17:47:36 +0100

    [DOC] Mise à jour journal avec les liens rouges

--- doc/journal.php
+++ doc/journal.php
@@ -44,7 +44,7 @@
                             </li>
 
                             <li class="table-row">
-                                <div class="table-cell"><span class="version">3.3.3</span></div>
+                                <div class="table-cell"><span class="version">3.3.4</span></div>
                                 <div class="table-cell">
                                     <ul>
                                         <li>Composants&nbsp;:
@@ -54,6 +54,7 @@
                                                 <li>Évolution&nbsp;: grille, ajout d’une classe <code>.su-filling-…</code> pour permettre d’utiliser un nombre de colonnes «&nbsp;implicite&nbsp;» inférieur à celui attendu par la grille.</li>
                                                 <li>Évolution&nbsp;: grille, possibilité de mettre les classes <code>.su-container</code> et <code>.su-row</code> au même niveau que <code>.sipaui</code>.</li>
                                                 <li>Évolution&nbsp;: marges, ajout d’une nouvelle taille de marge à 50&nbsp;px.</li>
+                                                <li>Évolution&nbsp;: liens éditoriaux OF passent en rouge.</li>
                                             </ul>
                                         </li>
                                     </ul>
